2ba85a328fa9a69fd8dbde73df15b46dc0aa8d34
Andy Zhang <andzhng@gmail.com>
2016-11-21 16:43:32 -0500

    Initial set up

---
+++ gallery.js
@@ -0,0 +1,9 @@
+var Gallery = function(config, opts) {
+  this._config = config;
+};
+
+// ...
+
+var config = null;
+var opts = {};
+new Gallery(config, opts);
---
+++ tools/sizes.js
@@ -0,0 +1,4 @@
+var sizeOf = require('image-size');
+var process = require('process');
+
+console.log(process.argv);
