b6e9b543a45f9571b0f6d154e14574fd65cba884
Andy Zhang <andzhng@gmail.com>
2016-11-21 19:35:24 -0500

    Modified the font, added shuffle photo

--- sizes.js
+++ sizes.js
@@ -1,19 +1,34 @@
 var sizeOf = require('image-size');
 var process = require('process');
 var fs = require('fs');
+var path = require('path');
 
 var config = {};
 
-for (var i = 2; i < process.argv.length; i++) {
-  var dirName = process.argv[i];
+var directories = fs.readdirSync('./').filter(function(file) {
+  return fs.statSync(path.join('./', file)).isDirectory();
+});
+
+for (var i = 0; i < directories.length; i++) {
+  var dirName = directories[i];
   var photos = [];
 
   var files = fs.readdirSync(dirName);
+  if (files.indexOf('settings.json') === -1) {
+    continue;
+  }
+
+  var settings = JSON.parse(fs.readFileSync(dirName + '/settings.json'));
+  if ('publish' in settings && !settings.publish) {
+    continue;
+  }
+
+
   for (var j = 0; j < files.length; j++) {
     var filename = files[j];
     if (!filename.endsWith('jpg')) { continue; }
 
-    var filePath = dirName + '/' + filename;
+    var filePath = dirName + (dirName.endsWith('/') ? '' : '/') + filename;
     var dimensions = sizeOf(filePath);
     photos.push({
       width: dimensions.width,
