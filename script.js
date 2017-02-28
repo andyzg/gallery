10cd01a76be190529c4892cafb1811654b157dcc
Andy Zhang <andzhng@gmail.com>
2017-02-27 22:52:01 -0800

    Imported all settings into _config.yml

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-var COLUMNS = 'columns';
-var ROWS = 'rows';
-var SQUARES = 'squares';
 var id = 'gallery';
-
-/** To customize the layout, CHANGE THE layoutStyle VARIABLE TO BE
-* COLUMNS: Column style layout
-* SQUARES: Square style layout, similar to Instagram
-* ROWS: Row style layout
-***/
-var layoutStyle = ROWS;
-var configuration = {
-  spacing: 10,
-  shuffle: false,
-  columns: 3,
-  maxHeight: 400
-}
 
 function reqListener() {
   var renderer
